740bd8260a3ddc4b194733aa0063bbc1b73b223c
samarpitaa24 <samarpita.kule@gmail.com>
2024-10-20 02:22:23 +0530

    module assignment

---
+++ assignments/module.py
@@ -0,0 +1,6 @@
+def rev(num) :
+        number =str(num)
+        reverse_str = number[::-1]
+        return int(reverse_str)
+
+
---
+++ assignments/p7.py
@@ -0,0 +1,5 @@
+import module
+
+number = int(input("enter numbers to be reversed : "))
+rev_num = module.rev(number)
+print(f"for {number} reversed number is ",rev_num )
